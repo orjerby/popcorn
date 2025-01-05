a796a7dfdc40a0706e954c14894ccd2901aea226
yarin <yarin@gmail.com>
2025-01-05 20:46:57 +0200

    updated home page

--- UI/src/pages/Home.tsx
+++ UI/src/pages/Home.tsx
@@ -10,9 +10,23 @@ function Home() {
           src="//www.pipsnacks.com/cdn/shop/t/205/assets/FRIES-Hero-tagline_headerR2-crop.png?v=144608968554861915091724934551"
           alt=""
         />
-        <Suspense fallback={<div>Loading products...</div>}>
-          <ProductList />
-        </Suspense>
+        <div className="flex h-128 items-center justify-center bg-white text-center text-black">
+          <p className="text-xl font-bold">
+            PIPCORN HEIRLOOM SNACKS IS FAMILY-FOUNDED – BORN FROM OUR LOVE OF
+            DELICIOUS SNACKS
+            <br />
+            AND OUR DESIRE FOR HEALTHIER ALTERNATIVES TO THE CLASSIC SNACKS WE
+            GREW UP WITH
+          </p>
+        </div>
+        <div className="py-12">
+          <h1 className="font-custom text-center text-5xl font-bold text-black">
+            Fan Favorites
+          </h1>
+          <Suspense fallback={<div>Loading products...</div>}>
+            <ProductList />
+          </Suspense>
+        </div>
       </div>
     </>
   )
