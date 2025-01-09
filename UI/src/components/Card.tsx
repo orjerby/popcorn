e2cfc2c806efd990b44196928ac64ceb2a597ef9
yarin <yarin@gmail.com>
2025-01-09 20:19:58 +0200

    added swiper to product list

--- UI/src/components/Card.tsx
+++ UI/src/components/Card.tsx
@@ -8,7 +8,7 @@ type CardProps = {
   reviewsCount: number
 }
 
-function Card({ image, title, size, price, reviewsCount }: CardProps) {
+function Card({ image, title, price, reviewsCount }: CardProps) {
   return (
     <div className="flex max-h-522 flex-col overflow-hidden">
       <div className="mt-37 flex max-h-481 max-w-268 flex-col items-center justify-center rounded-sm bg-white">
--- UI/src/containers/ProductList.tsx
+++ UI/src/containers/ProductList.tsx
@@ -1,11 +1,12 @@
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules'
+import { Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import Card from '../components/Card'
 import { useAppContext } from '../context/AppContext'
 import { selectProducts } from '../context/selectors'
 import { useData } from '../hooks/useData'
 import { getProducts } from '../services/productService'
-
+// import '../../node_modules/swiper/swiper-bundle.min.css'
+import 'swiper/swiper-bundle.css'
 export default function ProductList() {
   const { state, dispatch } = useAppContext()
   const products = selectProducts(state)
@@ -34,20 +35,17 @@ export default function ProductList() {
 
   return (
     <div className="h-550 overflow-hidden bg-[#f6f3e2] uppercase">
-      {/* <ul className="flex w-max gap-25"> */}
       <Swiper
         // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={6}
-        grabCursor={true}
-        navigation={true}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        modules={[Navigation]}
+        slidesPerView={'auto'}
+        spaceBetween={30}
+        navigation
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
       >
         {products.map((product) => (
-          <SwiperSlide>
+          <SwiperSlide className="!w-268" key={product.id}>
             <Card
               key={product.id}
               image={product.images[0]}
